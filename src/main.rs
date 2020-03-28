f938aac41db9e297c84a9e1f29dd4cece388e67d
Gordon Hart <gordon.hart2@gmail.com>
2020-03-28 11:43:34 -0700

    Use git ref for cpal instead of 0.11.0 release; continue familiarizing with cpal basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{anyhow, Result};
+use cpal::traits::{DeviceTrait, HostTrait};
+
+
+fn main() -> Result<()> {
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .ok_or_else(|| anyhow!("missing default output device"))?;
+    let config = device.default_output_config()?;
+    println!("supported: {:?}", device.supported_output_configs()?.collect::<Vec<cpal::SupportedStreamConfigRange>>());
+    println!("default: {:?}", config);
+
+    Ok(())
 }
